154cf497644a2d77f4346edc8f7679c7583ce925
philpinola1 <philpinola1@users.noreply.github.com>
2017-11-01 10:28:17 -0400

    Add files via upload

---
+++ photoresistor_Test.ino
@@ -0,0 +1,20 @@
+/*************************************************/
+const int photocellPin = A0;
+const int ledPin = 13;
+int outputValue = 0;
+const int res = 1023;
+/*************************************************/
+void setup()
+{
+Serial.begin(9600);
+}
+/*************************************************/
+void loop()
+{
+outputValue = analogRead(photocellPin);
+//outputValue = outputValue/1023;
+Serial.print("Value:  ");
+Serial.println(outputValue/2);
+delay(1000);
+}
+/*************************************************/
